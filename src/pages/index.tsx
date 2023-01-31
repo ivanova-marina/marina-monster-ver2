09aa0431a2f9082adbe43737f0ae3eb3368e0d5e
Marina Ivanova <marina0757@gmail.com>
2023-01-31 22:30:36 +0900

    testing tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p>Work in progress</p>
+        <p className="text-indigo-600">Work in progress</p>
       </main>
     </>
   );
