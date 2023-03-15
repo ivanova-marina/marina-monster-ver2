a9a78263c14441b934c6b4d8235a72eaae2b9719
Marina Ivanova <marina0757@gmail.com>
2023-03-15 18:39:02 +0900

    create getStaticPaths

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -1,3 +1,13 @@
+import { getAllArticlesId } from 'lib/articles';
+
+const getStaticPaths = () => {
+  const paths = getAllArticlesId();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 export default function Article() {
   return <></>;
 }
