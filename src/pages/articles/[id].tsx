65f647cd9c9f0780bb845b11c596730ec1874307
Marina Ivanova <marina0757@gmail.com>
2023-03-15 19:01:54 +0900

    create dynamic article route

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -1,13 +1,40 @@
-import { getAllArticlesId } from 'lib/articles';
+import { getAllArticlesId, getArticleData } from 'lib/articles';
 
-const getStaticPaths = () => {
+type ArticleDataProps = {
+  articleData: {
+    title: string;
+    id: string;
+    date: string;
+  };
+};
+
+type ParamsProps = {
+  params: {
+    id: string;
+  };
+};
+export default function Article({ articleData }: ArticleDataProps) {
+  return (
+    <>
+      <p>{articleData.title}</p>
+      <p>{articleData.id}</p>
+      <p>{articleData.date}</p>
+    </>
+  );
+}
+
+export async function getStaticProps({ params }: ParamsProps) {
+  const articleData = getArticleData(params.id);
+  return {
+    props: {
+      articleData,
+    },
+  };
+}
+export async function getStaticPaths() {
   const paths = getAllArticlesId();
   return {
     paths,
     fallback: false,
   };
-};
-
-export default function Article() {
-  return <></>;
 }
